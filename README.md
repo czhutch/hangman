d8043d7f772ee3e722cff11d85966a26afe707bc
czhutch <czhutch@roadrunner.com>
2021-12-01 17:41:18 -0500

    czh 2nd draft hangman

---
+++ README.md
@@ -0,0 +1,24 @@
+# __Christy's Hangman Game Project__
+**Version 1.0.0**
+
+## __Hangman Game Play Instructions:__
+#### 
+To start playing the hangman game, please follow these instructions:
+
+* Use the command line in your terminal and type node .     (node, space + .)  
+* Type 1 letter into the command line to guess a letter. Please guess only 1 letter at a time. 
+* It will not matter if you enter a letter as lowercase or uppercase, but you cannot enter characters.
+*  You will have 6 incorrect guesses to stay in the game to guess the word.
+*  For each incorrect guess, a 'man part' will appear, revealing the 'body' if you don't guess the word in 6 guesses.
+*  If you guess the word in less than 6 guesses, you won the round.  If you don't, you lost.
+*  You can exit/stop the game at anytime by typing ctrl c in the terminal.
+## __Installation__
+#### This game runs using the command line in Visual Studio Code.
+## __Contributions__
+#### This is a project for my portfolio. It is a 'work in progress' so I will be working on it over time to get it to 'work'. I've worked with my JS instructors (Matina & Jamal) to review my code. Otherwise, I should not take outside input on this project.
+## __Authors and Acknowledgement__
+#### Christy Hutchins is the author of this code. I did reference books on Javascript, game counters, YouTube videos, a google image search for making a drawing out of characters, and help from Matina (JS Instructor) to trouble shoot my first pass version. 
+## __License__
+#### This is an open source project. Please feel free to comment.
+## __Project Status__
+#### This is a work in progress! My submitted code does not currently work, but I will keep working on it!
--- index.js
+++ index.js
@@ -5,56 +5,64 @@ const getWord = () => {
     const word = wordBank[Math.floor(Math.random()* wordBank.length)]; 
     return word;
 };
-const maxGuess = 6;/*Do I need to set up a function for maxGuess or ref. in while loop?*/
-const count = 0;
-const countRounds = 0;
-const letterArray = () => {
-    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter => (.join(' '));
-}; /*Need to fix syntax error & understand what exactly is going on w/this .join is doing/working?. 
-Is 'letter' correct here or use input? or ?*/
-/*const getRandomWord = () => {
-    return word;
-}; --DO I need this? /* or aWord instead of word?*/
+
+const maxGuess = 6;
+const errors = 0;
+let count = 0;
+let userGuess = '@';
+
+/*Do I need a "start, message function HERE?"*/
+const isLetter = () => { /*Quokka Error Message "false ​​​​​at ​​​​​​​​isLetter()​​​ ​quokka.js:15:0​/"*/
+    const guess = userGuess[0];
+    return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(guess) > -1;
+}; 
+console.log(isLetter());
 
 const buildAnswerArray = (word) => {
-    const answerArray = []; /*This is making the word letters underscore. Does this need to be in a function, form?*/
+    const answerArray = []; 
     for (let i = 0; i < word.length; i++) {
         answerArray[i] = "_";
-    }
+    };
     return answerArray;
 };
-const remainingLetters = word.length;
 
-const guessStatus = (answerArray) => {  /*Need this? Or this is */
+const guessStatus = (answerArray) => {  
     return answerArray.join(" ");
 };
 
-const getGuess = () => {   /*Need this? Or this is same as const guess below?*/
-    if (remainingLetters > 0) {
-        console.log("Please guess 1 letter");
-        };
+const getGuess = () => {   
+    return prompt("Please guess 1 letter: ");   /*OK? or shld I use a console.log("Please guess 1 letter: ?")*/         
 };
 
-const gameStatus = (guess, word, answerArray) => {
-    const/* this is same as const guess below?
-    need to fill in function to update answerArray & return a number showing
-    # of x letter guess appears in the word so remainingLetters will update*/
+const gameStatus = (guess, word, answerArray) => {/*Are gameStatus & guess doing similar things-Need both?*/
+    let visual = 0;
+    for (var j = 0; j < word.length; j++) {
+        if (word[j] === guess) {
+            answerArray[j] = guess;
+            visual++;
+        }
+    } 
+    return visual;  
 };
+/*
+const maxGuess = () => {        Took out- Quokka error msg "maxGuess already declared"
+    if (errors >= maxGuess) {
+      return prompt("Round Over! The answer was" + word);  
+    }
+};  */
 
-const roundStatus = (numOfRounds) => { /*Need a for, of or in loop here? Going for Games Won X of total played*/
-    const game = roundStatus();
+const roundStatus = () => { /*Why is roundStatus 'void'? what do I need to return?*/
+    const round = maxGuess();  /*This may not be correct*/
     if (gameStatus === true) {
-    } else if (games)
-    console.log("Games won" + );
+        console.log("Games won" + gameStatus);
     } else {
         console.log("Press ctrl + c to stop play.");
     };
-
 };
+round(); /*This may not be correct*/
 
-const guess = () => {
+const guess = () => { /*Why is guess 'void'?--what do I need to return?*/
     if (guess === null) {
-        break;
     } else if (guess.length !== 1) {
         console.log("Please enter only a single letter");
     } else {
@@ -62,17 +70,21 @@ const guess = () => {
             if (word[j] === guess) {
                 answerArray[j] = guess;
                 remainingLetters--;
-            }
-        }
+            };
+        }; 
         count++; {            
-        }
+        };
         if (count > 6) {
-            return ("Game Over. The word was " + word);
-        }
-    };
+            console.log("Game Over. The word was " + word);
+        };
+};
+const word = getWord;
+const answerArray = buildAnswerArray;
+const remainingLetters = word.length;
+let winCount = 0;
+let total = 0;
 
-const displayGraphic = (guess) => {  /* shld ref back to guess, or make new, guessWrong?--need 2nd param,graphic?*/
-    /* Need a for in loop here?  Seems actually be working (Only thing :)...)*/
+const displayGraphic = (guess) => {  
    if (guess = 0) {
        return "__[[ ]]__";
    } else if (guess = 1) {
@@ -88,27 +100,32 @@ const displayGraphic = (guess) => {  /* shld ref back to guess, or make new, gue
    }
 };
 
+const round = () => {
+    if (gameStatus >= maxGuess()) {
+       /*  Need something here am sure*/
+    } 
+    let roundDisplay = "New Round!";
+    console.log(roundDisplay);
+    let word;  /* Have no idea what to 'declare' here, H/E know need to do something with winCount
+                 and total to display 'Games Won:' + round + ' of ' + total;*/
+}
+
 while (remainingLetters > 0) {
-    guessStatus(answerArray);
-    const guess = getGuess();    
+    guessStatus(answerArray);  /* Does this need to go after if/else statements?*/
+    const guess = getGuess();   /* Does this need to go after if/else statements?*/ 
     if (guess === null) {
-        displayGraphic();
+        displayGraphic(guess);
         break;
-    } else if (guess.length !==1) { /* why is there issue w/guess not ref. a value & ... under the .length?*/
-        console.log("Please enter 1 letter only");
+    } else if (guess !== 1) { 
+        console.log("Please enter 1 letter only");        
+    } else if (guess === true) {
+        getGuess();
     } else {
         let correctGuesses = gameStatus(guess, word, answerArray);
         remainingLetters -= correctGuesses;
         console.log("You won this round!");
     }
-   
-};
-
-console.log('Letter ' + letter); /*need or chg ref? */{
-count++;
-};
-if (count > 6) {  /* does this nee to go somewhere else?*/
     console.log("Round Over. The word was " + word);
-};
-
-    /*why do I have this red syntax error at end?*/
+    round();
+}};
+round();
